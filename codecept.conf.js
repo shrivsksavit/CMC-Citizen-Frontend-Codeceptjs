060dd2a8af3148f732bc84b9570e390d88ae1beb
Sumit Shrivastava <sumitsavit@hotmail.com>
2019-04-30 13:37:44 +0100

    added AAt and Local

--- codecept.conf.js
+++ codecept.conf.js
@@ -10,22 +10,22 @@ exports.config = {
   include: {
     I: './steps_file.js'
   },
-  //
-  // "helpers": {
-  //   "WebDriverIO": {
-  //     "url": "https://cmc-citizen-frontend-aat.service.core-compute-aat.internal/dashboard",
-  //     "browser": "chrome",
-  //     "desiredCapabilities": {
-  //       "proxy": {
-  //         "proxyType": "manual",
-  //         "httpProxy": "http://proxyout.reform.hmcts.net:8080",
-  //         "sslProxy": "http://proxyout.reform.hmcts.net:8080"
-  //         // "socksUsername": "my username",
-  //         // "socksPassword": "my password"
-  //       }
-  //     }
-  //   }
-  // },
+
+  "helpers": {
+    "WebDriverIO": {
+      "url": "https://cmc-citizen-frontend-aat.service.core-compute-aat.internal/dashboard",
+      "browser": "chrome",
+      "desiredCapabilities": {
+        "proxy": {
+          "proxyType": "manual",
+          "httpProxy": "http://proxyout.reform.hmcts.net:8080",
+          "sslProxy": "http://proxyout.reform.hmcts.net:8080"
+          // "socksUsername": "my username",
+          // "socksPassword": "my password"
+        }
+      }
+    }
+  },
   bootstrap: null,
   mocha: {},
   name: 'CodeceptJS'
